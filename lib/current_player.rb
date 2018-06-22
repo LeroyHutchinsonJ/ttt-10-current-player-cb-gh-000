8b51b8ea3fa2bfe442cf15f2589938e9b7808cd7
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-22 16:16:31 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,12 @@
-def turn_count
+counter = 0
+def turn_count(board,counter)
+board.do each|xorO|
 
 end
 
-def current_player(x = turn_count)
+end
+
+def current_player(x)
 if(x % 2 != 0)
   return "X"
 else
